c1c5c4248de3485f4c9b11c55cc04731c357ee20
Aliaksei Krasikau <Aliaksei_Krasikau@epam.com>
2015-05-21 16:10:00 +0300

    Change query to search only twits with phrases "#chess start" and "start #chess". Make little refactoring: - Removes unnecessary blocks of code. - Substitute use of series method with waterfall to call methods one after another. - Move query to the constant. - Add use of moment.js lib to work with date and time.

--- server.js
+++ server.js
@@ -1,12 +1,12 @@
 var express = require('express');
 var app = require("./wrio_app.js").init(express);
-var server = require('http').createServer(app).listen(3000);
+app.listen(3000);
 
 var nconf = require("./wrio_nconf.js").init();
 var Twit = require('twit');
 var async = require('async');
 
-// app.use(express.static(__dirname + '/public'));
+var moment = require('moment');
 
 var twconf = {
 	consumer_key: nconf.get("api:twitterLogin:consumerKey"),
@@ -14,108 +14,76 @@ var twconf = {
 	access_token: nconf.get("api:twitterLogin:access_token"),
 	access_token_secret: nconf.get("api:twitterLogin:access_token_secret")
 };
+
 console.log(twconf);
 var T = new Twit(twconf);
 
+var START_CHESS_QUERY = '"%23chess start" OR "start %23chess" since:' 
+		+ moment().format('YYYY-MM-DD');
 var last = 1;
-var dateTime = new Date();
-var queue = [];
-var idTwit = [];
 
-async.series([
-
-// ==========================
-// Search messages
-// ==========================
-
-	function(callback){
-		console.log('after');
-		setInterval(function (){
-
-				T.get('search/tweets', {
-					q: '#chess start since:' + dateTime.getFullYear()+ 
-					'-' + (dateTime.getMonth()+1) + '-' + dateTime.getDate(),
-					since_id: last
-				}, function(err, item) {
-					console.log("Searching from last ",last,"got ", item.statuses.length);
-					if (!item.statuses[0]) {
-					//last = 1;
-					//console.log ("--- reseting last");
-					} else{
-							for (var i = 0; i < item.statuses.length; i++) {
-								console.log ("Got id",item.statuses[i].id );
-								if (item.statuses[i].id > last) {
-									last = item.statuses[i].id.toString();
-									console.log("===Remembering last ",last);
-								}
-							}
+setInterval(function() {
+	T.get('search/tweets',  {
+		q: START_CHESS_QUERY,
+		since_id: last
+	}, function(err, item) {
+		console.log("Searching from last ", last, "got ", item.statuses.length);
+		if (item.statuses.length > 0) {
+				for (var i = 0; i < item.statuses.length; i++) {
+					console.log ("Got id",item.statuses[i].id );
+					if (item.statuses[i].id > last) {
+						last = item.statuses[i].id.toString();
+						console.log("===Remembering last ",last);
 					}
+				}
+		}
 
-				async.series([
-					function(callback){
-						for (var i = 0; i < item.statuses.length; i++){
+		async.waterfall([
+			function(callback){
+				var queue = [];
 
-							var textMessageUser = item.statuses[i].text;
+				for (var i = 0; i < item.statuses.length; i++){
 
-							console.log("Got tweet from", item.statuses[i].user.screen_name);
-							var regExpFind = /.*[\s#chess\s|\sstart\s]/;
-							var messageForUs = textMessageUser.match(regExpFind);
+					var textMessageUser = item.statuses[i].text;
 
-							if (messageForUs) {
-								console.log("Message 4 us");
-								var reply = {
-									textReply: '@' + item.statuses[i].user.screen_name +' Game started',
-									statusIdStrReply: item.statuses[i].id_str,
-									screenName: item.statuses[i].user.screen_name
-								};
+					console.log("Got tweet from", item.statuses[i].user.screen_name);
+					var regExpFind = /.*[\s#chess\s|\sstart\s]/;
+					var messageForUs = textMessageUser.match(regExpFind);
 
-								queue.push(reply);
-							}
-						}
-						callback(null, 'two');
-				},
-
-				// ==========================
-				// writeResult
-				// ==========================
-				function (callback) {
-					setInterval(function (){
-						for(var i = 0; i<queue.length;i++){
-							T.post('statuses/update', {
-								status: queue[i].textReply,
-								screen_name: queue[i].screenName,
-								in_reply_to_status_id: queue[i].statusIdStrReply
-							}, function (err,data,res){
-								if (err) {
-									console.log("Tweet send error ",err);
-									return;
-								}
-								console.log(data);
-
-								// resp.write(JSON.stringify(data));
-								queue = undefined;
-								queue=[];
-							});
+					if (messageForUs) {
+						console.log("Message 4 us");
+						var reply = {
+							textReply: '@' + item.statuses[i].user.screen_name +' Game started',
+							statusIdStrReply: item.statuses[i].id_str,
+							screenName: item.statuses[i].user.screen_name
 						};
 
-					callback(null, 'two');
-					queue = undefined;
-					queue=[];
+						queue.push(reply);
+					}
+				}
+				callback(null, queue);
+			},
 
-						},10000);
-				}
-				],
-				function(err, results){
-				    // results is now equal to ['one', 'two']
-				})
-				}
-			);
-	//	queue = undefined;
-	//queue=[];
-		},10000);
-	callback(null, 'two');
-}
-],
-function(err, results){
-    // results is now equal to ['one', 'two']
-});
+			// ==========================
+			// writeResult
+			// ==========================
+			function (queue, callback) {
+				for(var i = 0; i<queue.length;i++){
+					T.post('statuses/update', {
+						status: queue[i].textReply,
+						screen_name: queue[i].screenName,
+						in_reply_to_status_id: queue[i].statusIdStrReply
+					}, function (err,data,res){
+						if (err) {
+							console.log("Tweet send error ",err);
+							return;
+						}
+						console.log(data);
+					});
+				};
+				callback(null, 'two');
+			}
+		]);
+		}
+	);
+},10000);
