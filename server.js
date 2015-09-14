7efbd2223fec8a6f04dc4f9b5fbefc9d21beb035
Andrew <koreshkov.andrey@gmail.com>
2015-09-14 23:09:44 +0300

    Website #73
    https://github.com/webRunes/Chess-WRIO-Game/issues/73

--- server.js
+++ server.js
@@ -3,17 +3,12 @@ var nconf = require('./wrio_nconf.js');
 var express = require('express');
 var app = express();
 var db = require('./utils/db.js');
+var DOMAIN = nconf.get('server:workdomain');
 
 var session = require('express-session');
 var cookieParser = require('cookie-parser');
 var cookie_secret = nconf.get("server:cookiesecret");
 
-app.use(cookieParser(cookie_secret));
-app.use(session({
-	secret: cookie_secret,
-	saveUninitialized: true,
-	resave: false
-}));
 
 var mongoUrl = 'mongodb://' + nconf.get('mongo:user') + ':' + nconf.get('mongo:password') + '@' + nconf.get('mongo:host') + '/' + nconf.get('mongo:dbname');
 db.mongo({
@@ -27,9 +22,32 @@ db.mongo({
 			.listen(nconf.get("server:port"), function(req, res) {
 				console.log('app listening on port ' + nconf.get('server:port') + '...');
 				var wrioLogin = require('./wriologin')(db);
+
+				app.set('views', __dirname + '/views');
+				app.engine('htm', require('ejs')
+					.renderFile);
+				var SessionStore = require('connect-mongo')(session);
+				app.use(cookieParser(cookie_secret));
+				var sessionStore = new SessionStore({
+					db: db
+				});
+				app.use(session({
+
+					secret: cookie_secret,
+					saveUninitialized: true,
+					store: sessionStore,
+					resave: true,
+					cookie: {
+						secure: false,
+						domain: DOMAIN,
+						maxAge: 1000 * 60 * 60 * 24 * 30
+					},
+					key: 'sid'
+				}));
+
 				app.get('/', function(request, response) {
 					console.log(request.sessionID);
-					var render = 'index.ejs';
+					var render = '../index.htm';
 					wrioLogin.loginWithSessionId(request.sessionID, function(err, res) {
 						if (err) {
 							console.log("User not found:", err);
